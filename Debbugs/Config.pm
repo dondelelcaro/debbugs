01cb275a110b280a2b766cdfef0ea2050dbd1c6f
gecko <>
2000-03-20 16:24:33 -0800

    [project @ 2000-03-20 16:24:32 by gecko] Adding template tags for email and devel notes for anyone interested

--- Debbugs/Config.pm
+++ Debbugs/Config.pm
@@ -67,6 +67,8 @@ sub ParseConfigFile
     #parse config file
     print "V: Parsing Config File\n" if $Globals{ "verbose" };
     print "D3: Parse Config:\n@config\n" if $Globals{ 'debug' } > 2;
+    print "D1: Configuration\n" if $Globals{ 'debug' };
+
     for( my $i=0; $i<=$#config; $i++)
     {	$_ = $config[$i];
 	chop $_;
@@ -74,67 +76,137 @@ sub ParseConfigFile
 	next if /^#/;
 
 	if ( /^Email Domain\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'email-domain' } = strip( $1 ); }
+	{   $Globals{ 'email-domain' } = strip( $1 ); 
+	    $GTags{ 'EMAIL_DOMAIN' } = $Globals{ 'email-domain' };
+	    print "\tEmail Domain = $Globals{ 'email-domain' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^List Domain\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'list-domain' } = strip( $1 ); }
+	{   $Globals{ 'list-domain' } = strip( $1 ); 
+	    $GTags{ 'LIST_DOMAIN' } = $Globals{ 'list-domain' };
+	    print "\tList Domain = $Globals{ 'list-domain' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Web Domain\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'web-domain' } = strip( $1 ); }
+	{   $Globals{ 'web-domain' } = strip( $1 ); 
+	    $GTags{ 'WEB_DOMAIN' } = $Globals{ 'web-domain' };
+	    print "\tWeb Domain = $Globals{ 'web-domain' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^CGI Domain\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'cgi-domain' } = strip( $1 ); }
+	{   $Globals{ 'cgi-domain' } = strip( $1 ); 
+	    $GTags{ 'CGI_DOMAIN' } = $Globals{ 'cgi-domain' };
+	    print "\tCGI Domain = $Globals{ 'cgi-domain' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Short Name\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'project-short' } = strip( $1 ); }
+	{   $Globals{ 'project-short' } = strip( $1 ); 
+	    $GTags{ 'SHORT_NAME' } = $Globals{ 'project-short' };
+	    print "\tShort Name = $Globals{ 'project-short' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Long Name\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'project-long' } = strip( $1 ); }
+	{   $Globals{ 'project-long' } = strip( $1 ); 
+	    $GTags{ 'LONG_NAME' } = $Globals{ 'project-long' };
+	    print "\tLong Name = $Globals{ 'project-long' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Owner Name\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'owner-name' } = strip( $1 ); }
+	{   $Globals{ 'owner-name' } = strip( $1 ); 
+	    $GTags{ 'OWNER_NAME' } = $Globals{ 'owner-name' };
+	    print "\tOwner Name = $Globals{ 'owner-name' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Owner Email\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'owner-email' } = strip( $1 ); }
+	{   $Globals{ 'owner-email' } = strip( $1 ); 
+	    $GTags{ 'OWNER_EMAIL' } = $Globals{ 'owner-email' };
+	    print "\tOWNER Email = $Globals{ 'owner-email' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Spool Dir\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'spool-dir' } = strip( $1 ); }
+	{   $Globals{ 'spool-dir' } = strip( $1 ); 
+	    print "\tSpool Dir = $Globals{ 'spool-dir' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Work Dir\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'work-dir' } = strip( $1 ); }
+	{   $Globals{ 'work-dir' } = strip( $1 ); 
+	    print "\tWork Dir = $Globals{ 'work-dir' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Web Dir\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'www-dir' } = strip( $1 ); }
+	{   $Globals{ 'www-dir' } = strip( $1 ); 
+	    print "\tWeb Dir = $Globals{ 'www-dir' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Doc Dir\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'doc-dir' } = strip( $1 ); }
+	{   $Globals{ 'doc-dir' } = strip( $1 ); 
+	    print "\tDoc Dir = $Globals{ 'doc-dir' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Maintainer File\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'maintainer-file' } = strip( $1 ); }
+	{   $Globals{ 'maintainer-file' } = strip( $1 ); 
+	    print "\tMaintainer File = $Globals{ 'maintainer-file' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Submit List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'submit-list' } = strip( $1 ); }
+	{   $Globals{ 'submit-list' } = strip( $1 ); 
+	    $GTags{ 'SUBMIT_LIST' } = $Globals{ 'submit-list' };
+	    print "\tSubmit List = $Globals{ 'submit-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Maint List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'maint-list' } = strip( $1 ); }
+	{   $Globals{ 'maint-list' } = strip( $1 ); 
+	    $GTags{ 'MAINT_LIST' } = $Globals{ 'maint-list' };
+	    print "\tMaint List = $Globals{ 'maint-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Quiet List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'quiet-list' } = strip( $1 ); }
+	{   $Globals{ 'quiet-list' } = strip( $1 ); 
+	    $GTags{ 'QUIET_LIST' } = $Globals{ 'quiet-list' };
+	    print "\tQuiet List = $Globals{ 'quiet-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Forwarded List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'forwarded-list' } = strip( $1 ); }
+	{   $Globals{ 'forwarded-list' } = strip( $1 ); 
+	    $GTags{ 'FORWARDED_LIST' } = $Globals{ 'forwarded-list' };
+	    print "\tForwarded List = $Globals{ 'forwarded-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Done List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'done-list' } = strip( $1 ); }
+	{   $Globals{ 'done-list' } = strip( $1 ); 
+	    $GTags{ 'DONE_LIST' } = $Globals{ 'done-list' };
+	    print "\tDone List = $Globals{ 'done-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Request List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'request-list' } = strip( $1 ); }
+	{   $Globals{ 'request-list' } = strip( $1 ); 
+	    $GTags{ 'REQUEST_LIST' } = $Globals{ 'request-list' };
+	    print "\tRequest List = $Globals{ 'request-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Submitter List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'submitter-list' } = strip( $1 ); }
+	{   $Globals{ 'submitter-list' } = strip( $1 ); 
+	    $GTags{ 'SUBMITTER_LIST' } = $Globals{ 'submitter-list' };
+	    print "\tSubmitter List = $Globals{ 'submitter-list' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Control List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'control-list' } = strip( $1 ); }
+	{   $Globals{ 'control-list' } = strip( $1 ); 
+	    $GTags{ 'CONTROL_LIST' } = $Globals{ 'control-list' };
+	    print "\tControl List = $Globals{ 'control-list' }\n" if $Globals{ 'debug' };
+	    $GTags{ '' } = $Globals{ '' };
+	    print "\t = $Globals{ '' }\n" if $Globals{ 'debug' };
+	}
 	elsif ( /^Summary List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'summary-list' } = strip( $1 ); }
+	{   $Globals{ 'summary-list' } = strip( $1 ); 
+	}
 	elsif ( /^Mirror List\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'mirror-list' } = strip( $1 ); }
+	{   $Globals{ 'mirror-list' } = strip( $1 ); 
+	}
 	elsif ( /^Mailer\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'mailer' } = strip( $1 ); }
+	{   $Globals{ 'mailer' } = strip( $1 ); 
+	}
 	elsif ( /^Singular Term\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'singular' } = strip( $1 ); }
+	{   $Globals{ 'singular' } = strip( $1 ); 
+	}
 	elsif ( /^Plural Term\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'plural' } = strip( $1 ); }
+	{   $Globals{ 'plural' } = strip( $1 ); 
+	}
 	elsif ( /^Expire Age\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'expire-age' } = strip( $1 ); }
+	{   $Globals{ 'expire-age' } = strip( $1 ); 
+	}
 	elsif ( /^Save Expired Bugs\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'save-expired' } = strip( $1 ); }
+	{   $Globals{ 'save-expired' } = strip( $1 ); 
+	}
 	elsif ( /^Mirrors\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'mirrors' } = strip( $1 ); }
+	{   $Globals{ 'mirrors' } = strip( $1 ); 
+	}
 	elsif ( /^Default Severity\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'default-severity' } = strip( $1 ); }
+	{   $Globals{ 'default-severity' } = strip( $1 ); 
+	}
 	elsif ( /^Normal Severity\s*[:=]\s*([^#]*)/i )
-	{ $Globals{ 'normal-severity' } = strip( $1 ); }
+	{   $Globals{ 'normal-severity' } = strip( $1 ); 
+	}
         elsif ( /^Severity\s+#*(\d+)\s*[:=]\s*([^#]*)/i )
         {   $Severity{ $1 } = $2;
             print "D2: (config) Severity $1=$Severity{$1}\n" if $Globals{ 'debug' } > 1;
