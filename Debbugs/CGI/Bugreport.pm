faa25bd6ba58375ba6c28bd7b66b463c21372f06
Don Armstrong <don@donarmstrong.com>
2013-03-13 18:12:12 -0700

    add libravatar support to the bts

--- Debbugs/CGI/Bugreport.pm
+++ Debbugs/CGI/Bugreport.pm
@@ -32,6 +32,8 @@ use base qw(Exporter);
 
 use IO::Scalar;
 use Params::Validate qw(validate_with :types);
+use Digest::MD5 qw(md5_hex);
+use Debbugs::Mail qw(get_addresses);
 use Debbugs::MIME qw(convert_to_utf8 decode_rfc1522 create_mime_message);
 use Debbugs::CGI qw(:url :html :util);
 use Debbugs::Common qw(globify_scalar english_join);
@@ -132,19 +134,22 @@ sub display_entity {
 	not $param{terse} and
 	not exists $param{att}) {
 	 my $header = $entity->head;
-	 print {$param{output}} "<pre class=\"headers\">\n";
+	 print {$param{output}} "<div class=\"headers\">\n";
 	 if ($param{trim_headers}) {
 	      my @headers;
 	      foreach (qw(From To Cc Subject Date)) {
 		   my $head_field = $head->get($_);
 		   next unless defined $head_field and $head_field ne '';
-		   push @headers, qq(<b>$_:</b> ) . html_escape(decode_rfc1522($head_field));
+                   if ($_ eq 'From') {
+                       push @headers,q(<img src=").__libravatar_url(decode_rfc1522($head_field)).q(">);
+                   }
+		   push @headers, qq(<p><span class="header">$_:</span> ) . html_escape(decode_rfc1522($head_field))."</p>";
 	      }
 	      print {$param{output}} join(qq(), @headers);
 	 } else {
-	      print {$param{output}} html_escape(decode_rfc1522($entity->head->stringify));
+	      print {$param{output}} "<pre>".html_escape(decode_rfc1522($entity->head->stringify))."</pre>\n";
 	 }
-	 print {$param{output}} "</pre>\n";
+	 print {$param{output}} "</div>\n";
     }
 
     if (not (($param{outer} and $type =~ m{^text(?:/plain)?(?:;|$)})
@@ -424,6 +429,12 @@ sub handle_record{
      return $output;
 }
 
+
+sub __libravatar_url {
+    my ($email) = @_;
+    ($email) = get_addresses($email);
+    return "http://cdn.libravatar.org/avatar/".md5_hex(lc($email))."?d=retro";
+}
 
 
 1;
