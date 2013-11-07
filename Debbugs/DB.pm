09a8383c68b83153062f79e8b57571e486b19507
Don Armstrong <don@donarmstrong.com>
2013-11-06 21:21:15 -0800

    set the version to 1; eventually we will use this to update schemas

--- Debbugs/DB.pm
+++ Debbugs/DB.pm
@@ -15,6 +15,9 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07025 @ 2012-07-17 10:25:29
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wiMg1t5hFUhnyufL3yT5fQ
 
+# This version must be incremented any time the schema changes so that
+# DBIx::Class::DeploymentHandler can do its work
+our $VERSION=1;
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
