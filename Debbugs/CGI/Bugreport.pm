4723d91a6b37571119d57413b3eef4b47d1e3d73
Don Armstrong <don@donarmstrong.com>
2014-02-07 12:06:51 -0800

    switch the libravatar url to not prefix with http://

--- Debbugs/CGI/Bugreport.pm
+++ Debbugs/CGI/Bugreport.pm
@@ -149,7 +149,7 @@ sub display_entity {
                    if ($_ eq 'From' and $param{avatars}) {
                        my $libravatar_url = __libravatar_url(decode_rfc1522($head_field));
                        if (defined $libravatar_url and length $libravatar_url) {
-                           push @headers,q(<img src="http://).html_escape($libravatar_url).qq(" alt="">\n);
+                           push @headers,q(<img src=").html_escape($libravatar_url).qq(" alt="">\n);
                        }
                    }
 		   push @headers, qq(<div class="header"><span class="headerfield">$_:</span> ) . html_escape(decode_rfc1522($head_field))."</div>\n";
