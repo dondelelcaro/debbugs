c0985911f262b76ff9726710a5666303e8fc8856
Don Armstrong <don@donarmstrong.com>
2014-11-29 13:04:34 -0800

    allow specifying DEBBUGS_SERVICE for dbicdump_command.sh

--- sql/dbicdump_command.sh
+++ sql/dbicdump_command.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "$DEBBUGS_SERVICE" ]; then
+    DEBBUGS_SERVICE="debbugs";
+fi;
+
 if ! [ -d "sql" ] || ! [ -d "Debbugs" ]; then 
     echo "In the wrong directory"
     exit 1;
@@ -8,5 +12,5 @@ fi;
 dbicdump -I. -o dump_directory=. \
     -o components='["InflateColumn::DateTime"]' \
     -o preserve_case=1 \
-    Debbugs::DB dbi:Pg:service=debbugs '' '';
+    Debbugs::DB dbi:Pg:service=$DEBBUGS_SERVICE '' '';
 
