ea4d4f53f6a4de569a3bd07f6cf29e8f771f0b99
Don Armstrong <don@donarmstrong.com>
2017-02-26 07:42:22 -0800

    document Debbugs::DB::ResultSet

--- Debbugs/DB/ResultSet/Maintainer.pm
+++ Debbugs/DB/ResultSet/Maintainer.pm
@@ -27,7 +27,20 @@ use base 'DBIx::Class::ResultSet';
 use Debbugs::DB::Util qw(select_one);
 
 
+=over
 
+=item get_maintainers 
+
+     $s->resultset('Maintainers')->get_maintainers();
+
+     $s->resultset('Maintainers')->get_maintainers(@maints);
+
+Retrieve a HASHREF of all maintainers with the maintainer name as the key and
+the id of the database as the value. If given an optional list of maintainers,
+adds those maintainers to the database if they do not already exist in the
+database.
+
+=cut
 sub get_maintainers {
     my ($self,@maints) = @_;
     my %maints;
@@ -52,6 +65,16 @@ sub get_maintainers {
     @maints{@maint_names} = @maint_ids;
     return \%maints;
 }
+
+=item get_maintainer_id
+
+     $s->resultset('Maintainer')->get_maintainer_id('Foo Bar <baz@example.com>')
+
+Given a maintainer name returns the maintainer id, possibly inserting the
+maintainer (and correspondent) if either do not exist in the database.
+
+
+=cut
 
 sub get_maintainer_id {
     my ($self,$maint) = @_;
@@ -84,6 +107,10 @@ SQL
 	       $maint,$ci
 	      );
 }
+
+=back
+
+=cut
 
 1;
 
